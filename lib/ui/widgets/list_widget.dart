5a7b9b1ab7530e36c1c2bf0ed13108a33ea6b152
kevin <kevinwu352@gmail.com>
2025-08-03 20:07:43 +0800

    012

--- lib/ui/widgets/list_widget.dart
+++ lib/ui/widgets/list_widget.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ListWidget extends StatelessWidget {
-  const ListWidget({
-    super.key,
-    this.color,
-    this.padding,
-    this.header,
-    this.footer,
-    required this.children,
-  });
+  const ListWidget({super.key, this.color, this.padding, this.header, this.footer, required this.children});
 
   final Color? color;
   final EdgeInsetsGeometry? padding;
@@ -18,44 +11,26 @@ class ListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final list = ListView(
-      physics: BouncingScrollPhysics(),
-      padding: padding,
-      children: children,
-    );
-    if (footer is Widget || footer is Widget) {
-      if (color is Color) {
-        return Container(
-          color: color,
-          child: SafeArea(
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(color: color),
+        if (footer is Widget || footer is Widget)
+          SafeArea(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 ?header,
-                Expanded(child: list),
+                Expanded(
+                  child: ListView(physics: BouncingScrollPhysics(), padding: padding, children: children),
+                ),
                 ?footer,
               ],
             ),
-          ),
-        );
-      } else {
-        return SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ?header,
-              Expanded(child: list),
-              ?footer,
-            ],
-          ),
-        );
-      }
-    } else {
-      if (color is Color) {
-        return Container(color: color, child: list);
-      } else {
-        return list;
-      }
-    }
+          )
+        else
+          ListView(physics: BouncingScrollPhysics(), padding: padding, children: children),
+      ],
+    );
   }
 }
--- lib/ui/widgets/scroll_widget.dart
+++ lib/ui/widgets/scroll_widget.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ScrollWidget extends StatelessWidget {
-  const ScrollWidget({
-    super.key,
-    this.color,
-    this.padding,
-    this.header,
-    this.footer,
-    required this.children,
-  });
+  const ScrollWidget({super.key, this.color, this.padding, this.header, this.footer, required this.children});
 
   final Color? color;
   final EdgeInsetsGeometry? padding;
@@ -18,57 +11,29 @@ class ScrollWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (footer is Widget || footer is Widget) {
-      if (color is Color) {
-        return Container(
-          color: color,
-          child: SafeArea(
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(color: color),
+        if (footer is Widget || footer is Widget)
+          SafeArea(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 ?header,
                 Expanded(
-                  child: SingleChildScrollView(
-                    padding: padding,
-                    child: Column(children: children),
-                  ),
+                  child: SingleChildScrollView(child: Column(children: children)),
                 ),
                 ?footer,
               ],
             ),
-          ),
-        );
-      } else {
-        return SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ?header,
-              Expanded(
-                child: SingleChildScrollView(
-                  padding: padding,
-                  child: Column(children: children),
-                ),
-              ),
-              ?footer,
-            ],
-          ),
-        );
-      }
-    } else {
-      return LayoutBuilder(
-        builder: (context, constraints) => Container(
-          color: color,
-          constraints: BoxConstraints(
-            minWidth: constraints.maxWidth,
-            minHeight: constraints.maxHeight,
-          ),
-          child: SingleChildScrollView(
+          )
+        else
+          SingleChildScrollView(
             padding: padding,
             child: SafeArea(child: Column(children: children)),
           ),
-        ),
-      );
-    }
+      ],
+    );
   }
 }
