5250a50fa95b13f7e79d560aa760f80b1ad1fedd
kevin <kevinwu352@gmail.com>
2025-09-15 10:34:15 +0800

    052

--- lib/core/collection_ext.dart
+++ lib/core/collection_ext.dart
@@ -1,13 +1,31 @@
 part of 'core.dart';
 
 extension MapExt<K, V> on Map<K, V> {
-  // void put(K k, V? v) {
-  //   if (v is V) {
-  //     this[k] = v;
-  //   } else {
-  //     remove(k);
-  //   }
-  // }
+  void setValue(K k, V? v) {
+    if (v is V) {
+      this[k] = v;
+    } else {
+      remove(k);
+    }
+  }
+
+  Map<String, V> keyed() {
+    if (this is Map<String, V>) {
+      return cast<String, V>();
+    } else {
+      return Map.fromEntries(
+        entries.map((e) => MapEntry(e.key is String ? e.key : null, e.value)).where((e) => e.key != null),
+      ).cast<String, V>();
+    }
+  }
+
+  Map<String, V> toKeyed() {
+    if (this is Map<String, V>) {
+      return cast<String, V>();
+    } else {
+      return map((k, v) => MapEntry(k is String ? k : k.toString(), v));
+    }
+  }
 
   Map<K, V> where(bool Function(K k, V v) test) => Map.fromEntries(entries.where((e) => test(e.key, e.value)));
 
@@ -20,9 +38,4 @@ extension MapExt<K, V> on Map<K, V> {
 
 extension IterableExt<T> on Iterable<T> {
   Iterable<R> compactMap<R>(R? Function(T e) transform) => map((e) => transform(e)).whereType<R>();
-}
-
-void foo() {
-  final map = {'a': 1};
-  // map.compactMap()
 }
--- lib/utils/map_ext.dart
+++
@@ -1,9 +0,0 @@
-extension MapExt<K, V> on Map<K, V> {
-  void put(K k, V? v) {
-    if (v is V) {
-      this[k] = v;
-    } else {
-      remove(k);
-    }
-  }
-}
